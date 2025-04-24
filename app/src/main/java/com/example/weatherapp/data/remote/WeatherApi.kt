df4dd8856eb13ec0e1ee4db891bf3aee23eff833
IBRAHIM-ABDEEN\Ibrahim <ibrahim.abdeen@atis.al>
2025-04-24 13:50:04 +0200

    Added necessary dependencies

--- app/src/main/java/com/example/weatherapp/data/remote/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapp/data/remote/WeatherApi.kt
@@ -1,9 +1,12 @@
 package com.example.weatherapp.data.remote
 
-interface WeatherApi {
-    suspend fun getWeatherData(
-        @Query("")latitude: Double,
-        @Query("")longitude: Double
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface WeatherApi {
+    @GET("v1/forecast?hourly=temperature_2m&current=temperature_2m,weather_code,relative_humidity_2m,wind_speed_10m,pressure_msl")
+    suspend fun getWeatherData(
+        @Query("") latitude: Double,
+        @Query("") longitude: Double
     )
 }
